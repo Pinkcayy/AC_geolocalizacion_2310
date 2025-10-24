f7770d83cbab339dc6d3c3d5f0d1e929fe1f6436
Pinkcayy <karenc112358@gmail.com>
2025-10-23 21:14:29 -0400

    avance tarea

---
+++ app/src/main/java/com/example/ac_geolocalizacion_2310/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.ac_geolocalizacion_2310;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap myMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager(). findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        myMap = googleMap;
+        LatLng santaCruz = new LatLng(-17.724273931936406, -63.15568287015286);
+        //myMap.addMarker(new MarkerOptions().position(santaCruz).title("Santa Cruz Elay"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(santaCruz));
+        MarkerOptions options = new MarkerOptions().position(santaCruz).title("Santa Cruz Puej");
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        myMap.addMarker(options);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture)
+    {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
